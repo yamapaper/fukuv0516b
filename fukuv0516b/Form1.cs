124c56f54bc1f679dd0e9512db35852eeac608b1
yamada kenta <yamapaper2234@gmail.com>
2025-05-16 16:00:52 +0900

    Initial commit

---
+++ fukuv0516b/Form1.cs
@@ -0,0 +1,35 @@
+namespace fukuv0516b
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top -= 20;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left -= 20;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left += 20;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top -= 20;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "ŽR“cŒ’‘¾";
+        }
+    }
+}
